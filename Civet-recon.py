2efe2d9ca63d27509c7b9a156c427d5d32461554
Mr-C0k1 <yudiahmad8080@gmail.com>
2025-07-17 05:34:09 -0400

    Add files via upload

---
+++ Civet-recon.py
@@ -0,0 +1,22 @@
+
+#!/usr/bin/env python3
+# Civet-recon.py - Reconnaissance & Exploitation Tool
+
+def print_logo():
+    logo = r"""
+     ／＞　 フ
+     | 　_　_| 
+   ／` ミ＿xノ 
+  /　　　　 |
+ /　 ヽ　　 ﾉ
+│　　|　|　|
+／￣|　　 |　|
+(￣ヽ＿_ヽ_)__)
+＼二)
+      CIVET Recon Engine
+    """
+    print(logo)
+
+print_logo()
+
+# ... lanjutkan dengan argparser, target parsing, stealth mode, scanning API, dll.
